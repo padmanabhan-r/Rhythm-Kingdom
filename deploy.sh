bd0b89b4db507993e0e5ef3bd6ac448244007387
Padmanabhan <padmanabhan.rajendrakumar@gmail.com>
2026-04-30 19:57:20 +0530

    chore: deploy to production branch by default

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 echo "Deploying Rhythm Kingdom to Cloudflare Pages..."
-wrangler pages deploy . --project-name rhythm-kingdom --commit-dirty=true
+wrangler pages deploy . --project-name rhythm-kingdom --branch main --commit-dirty=true
